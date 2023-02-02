d0dbc431eba2cb0fe54d42b8825d51cbccb7e8aa
Tamara Slosarek <tamara.slosarek@hpi.de>
2023-02-02 13:22:45 +0100

    Improve comparing script

--- genomics-lecture-specific/compare-generated-data.sh
+++ genomics-lecture-specific/compare-generated-data.sh
@@ -1,8 +1,9 @@
 new_directory="AllData"
 old_directory="_AllData"
-for old_file in data/${old_directory}/* results/${old_directory}/*
+
+for old_file in data/${old_directory}/* results/${old_directory}/* results/${old_directory}/*/* 
 do
-  if [[ $old_file == *.csv ]]
+  if [[ $old_file == *.csv && $old_file != */Q*.csv && $old_file != *sankey* && $old_file != *stacked-bar* ]]
   then
     new_file=${old_file/$old_directory/$new_directory}
     if [[ -f $new_file ]]
@@ -11,6 +12,7 @@ do
       if [[ $result != "" ]]
       then
         echo "⚠️  $result"
+        echo "ℹ️  Show details by running 'diff $old_file $new_file'"
       else
         echo "✅ Files $old_file and $new_file match"
       fi
